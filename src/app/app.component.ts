d3ddd7f205df43fab17ce48b68b5dcbafef775dd
Raviteja Gubba <raviteja@kobler.no>
2019-03-14 23:01:32 +0100

    Used css grid for layout and angular for user input validation and so on

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'haveFunWithPi';
+  pi = "3.1415926535 897932384626 4338327950288 4197169399375 1058209749445 92307816406286 2089986280348 253421170679821 4808651328230";
+  piAim = "3.141592653589793238462643383279502884197169399375105820974944592307816406286 20899862803482534211706798214808651328230";
+  piInput = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+  pi100 = ["1415926535", "897932384626", "4338327950288", "4197169399375", "1058209749445", "92307816406286", "2089986280348",
+    "253421170679821", "4808651328230"];
+  pi100UserInput = [];
+
+  tranlsate() {
+    return this.pi.replace(/[0123456789]/g, m => this.piInput[m]);
+  }
+
+  isValueCorrect(input, fact){
+    return input == fact;
+  }
 }
